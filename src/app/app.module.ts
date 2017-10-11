d811da2ed08e16a472b2ecc71c63f45064980aad
anishpanthi <anish.panthi@gmail.com>
2017-10-10 20:54:31 -0500

    Necessary Components Created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { CreateUserComponent } from './create-user/create-user.component';
+import { SearchUserComponent } from './search-user/search-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserDetailsComponent,
+    CreateUserComponent,
+    SearchUserComponent
   ],
   imports: [
     BrowserModule
